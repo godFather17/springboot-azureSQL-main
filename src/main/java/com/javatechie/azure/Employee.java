af36857fc244ad08f3eba96e7a144b1d3f278b59
mohamed <afif.mohamed@gmail.com>
2022-12-26 14:08:35 +0100

    first commit

---
+++ src/main/java/com/javatechie/azure/Employee.java
@@ -0,0 +1,66 @@
+package com.javatechie.azure;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+	
+	@Column(name = "name")
+    private String name;
+	
+	@Column(name = "dept")
+    private String dept;
+	
+	@Column(name = "salary")
+    private long salary;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	
+	
+}
+
+
